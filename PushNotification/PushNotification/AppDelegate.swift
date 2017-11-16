24d80479685bb5726f1255299a36e5f77fc75807
vishal <vishalkalola196@gmail.com>
2017-11-16 17:10:04 +0530

    All module Uploaded

---
+++ PushNotification/PushNotification/AppDelegate.swift
@@ -0,0 +1,122 @@
+//
+//  AppDelegate.swift
+//  PushNotification
+//
+//  Created by vishal on 10/11/17.
+//  Copyright © 2017 vishal. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 10, *)
+        { // iOS 10 support
+            //create the notificationCenter
+            let center = UNUserNotificationCenter.current()
+            center.delegate = self
+            // set the type as sound or badge
+            center.requestAuthorization(options: [.sound,.alert,.badge]) { (granted, error) in
+                if granted {
+                    print("Notification Enable Successfully")
+                }else{
+                    print("Some Error Occure")
+                }
+            }
+            application.registerForRemoteNotifications()
+        }
+        else if #available(iOS 9, *)
+        {
+            // iOS 9 support
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        else if #available(iOS 8, *)
+        {
+            // iOS 8 support
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound,
+                                                                                                     .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        else
+        { // iOS 7 support
+            application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
+        }
+        return true
+    }
+    
+    
+    //get device token here
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken
+        deviceToken: Data)
+    {
+        var token = ""
+        for i in 0..<deviceToken.count {
+            token = token + String(format: "%02.2hhx", arguments: [deviceToken[i]])
+        }
+        print("Registration succeeded!")
+        print("Token: ", token)
+    }
+    
+    //get error here
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error:
+        Error) {
+        print("Registration failed!")
+    }
+    
+    //get Notification Here below ios 10
+    func application(_ application: UIApplication, didReceiveRemoteNotification data: [AnyHashable : Any]) {
+        // Print notification payload data
+        print("Push notification received: \(data)")
+    }
+    
+    //This is the two delegate method to get the notification in iOS 10..
+    //First for foreground
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (_ options:UNNotificationPresentationOptions) -> Void)
+    {
+        print("Handle push from foreground")
+        // custom code to handle push while app is in the foreground
+        print("\(notification.request.content.userInfo)")
+    }
+    //Second for background and close
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response:UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
+    {
+        print("Handle push from background or closed")
+        // if you set a member variable in didReceiveRemoteNotification, you will know if this is from closed or background
+        print("\(response.notification.request.content.userInfo)")
+    }
+    
+    
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
